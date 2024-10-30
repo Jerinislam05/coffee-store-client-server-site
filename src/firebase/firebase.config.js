fd6bc40c94d27f11e251eaab88440de11e835352
Jerinislam05 <jerinislam05@gmail.com>
2024-10-31 04:04:04 +0600

    firebase context recap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn_-tygMcKmDNy_8AuMCaFdKTTVmFIK28",
+  authDomain: "coffee-store-744fd.firebaseapp.com",
+  projectId: "coffee-store-744fd",
+  storageBucket: "coffee-store-744fd.firebasestorage.app",
+  messagingSenderId: "534586263924",
+  appId: "1:534586263924:web:205bd5caa33b8594400421",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
